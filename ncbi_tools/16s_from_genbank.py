d8751aa366b5c4d38616c0249d3948b526dabc51
Jin Choi <jinchoi721@gmail.com>
2016-01-21 07:39:42 -0600

    ncbi tools

---
+++ ncbi_tools/16s_from_genbank.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+def get_16s(genbankname):
+    genome=SeqIO.read(genbankname, 'genbank')
+
+    n = 0
+    l = []
+
+    for record in list(SeqIO.parse(genbankname, 'genbank')):
+        org = record.annotations["source"]
+        for feat in genome.features:
+            if feat.type == "rRNA":
+                if '16S' in feat.qualifiers['product'][0]:#or '16S ribosomal' for strict match
+                    start = feat.location.start.position
+                    end = feat.location.end.position
+                    pos = [start, end]
+                    print pos
+                    l.append(pos)
+                    print '>' + genbankname.split('.')[0] + org + ' '+ '16S rRNA gene' + str(n)                
+                    print feat.extract(genome.seq)
+                    n =+ 1
+    return n
+
+def main():
+    file = open(sys.argv[1],'r')
+    for line in file:
+        genbankname = line.strip()+".gbk"
+        print genbankname
+        get_16s(genbankname)
+
+if __name__ == '__main__':
+    main()

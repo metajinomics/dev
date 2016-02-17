a990a103dcd50e8f56df369fefa8d6548e5f5852
Jin Choi <jinchoi721@gmail.com>
2016-02-17 14:39:43 -0600

    update

---
+++ ncbi_tools/genbank_to_organism.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import sys
+import genbank_get
+
+fread = open(sys.argv[1],'r')
+locus = ""
+for gbline in fread:
+    if ("LOCUS" in gbline):
+        locus = gbline.strip().split(" ")[7]
+    if("organism=" in gbline):
+        organism = gbline.strip().split("=")[1][1:-1]
+    if(gbline.strip()[:2] == "//" ):
+        print locus+" "+organism
+        locus = ""
+        organism = ""

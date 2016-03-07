3f58398525fa2ef572df2be5a5d7a281da711298
Jin Choi <jinchoi721@gmail.com>
2016-03-07 13:30:25 -0600

    update;

--- emp_tools/make_abun_table.py
+++ emp_tools/make_abun_table.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# python make_abun_table.py soil_type_count.txt refsoil_emp_blast.out.txt
+# python make_abun_table.py soil_type_count.txt refsoil_emp_blast.out.txt > soil_type_abun.txt
 
 import sys
 
@@ -7,7 +7,8 @@ dict = {}
 for line in open(sys.argv[1],'r'):
     spl = line.strip().split('\t')
     dict[spl[0]]=line.strip()
-
+temp = ["otu","Andisols","Gelisols","Vertisols","Mollisols","Inceptisols","Alfisols","Ultisols","Sand,Rock,Ice","Entisols"]
+print '\t'.join(temp)
 for line in open(sys.argv[2],'r'):
     spl = line.strip().split('\t')
     if (dict.has_key(spl[1])):

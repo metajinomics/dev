39f8b9a686d180bdc4ecfb23f78b0d81b12d1a72
Jin Choi <jinchoi721@gmail.com>
2016-03-07 13:39:55 -0600

    update

--- emp_tools/make_abun_table.py
+++ emp_tools/make_abun_table.py
@@ -7,7 +7,7 @@ dict = {}
 for line in open(sys.argv[1],'r'):
     spl = line.strip().split('\t')
     dict[spl[0]]=line.strip()
-temp = ["otu","Andisols","Gelisols","Vertisols","Mollisols","Inceptisols","Alfisols","Ultisols","Sand,Rock,Ice","Entisols"]
+temp = ["otu","taxID","Andisols","Gelisols","Vertisols","Mollisols","Inceptisols","Alfisols","Ultisols","Sand,Rock,Ice","Entisols"]
 print '\t'.join(temp)
 for line in open(sys.argv[2],'r'):
     spl = line.strip().split('\t')

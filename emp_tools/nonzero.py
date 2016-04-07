c12d19e595b87838e505e3e0fc0e34f6417f72bf
Jin Choi <jinchoi721@gmail.com>
2016-04-07 11:06:38 -0500

    update

--- emp_tools/nonzero.py
+++ emp_tools/nonzero.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-#usage: python nonzero.py biom
+#usage: python nonzero.py biom > nonzero.out
 from biom import load_table
 from biom.table import Table
 import numpy as np
 import sys
 
 table = load_table(sys.argv[1])
-a = table.nonzero_counts('observation',True)
-b = table.ids(axis='observation')
-print len(a)
-print len(b)
+count = table.nonzero_counts('observation',True)
+otuid = table.ids(axis='observation')
+#print len(a)
+#print len(b)
 for i in range(0,len(a)):
-    print a[i],b[i]
+    print otuid[i],count[i]

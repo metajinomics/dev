f348c3d9e2b1581c7ec42a3ff34d60e3cb9f2e89
Jin Choi <jinchoi721@gmail.com>
2016-05-03 12:27:43 -0500

    update

---
+++ ncbi_tools/fetch_protein.py
@@ -0,0 +1,21 @@
+from Bio import SeqIO
+import sys
+gb_file = sys.argv[1]
+gb_record = SeqIO.read(open(gb_file,'r'),'genbank')
+#print "Name %s, %i features" % (gb_record.name, len(gb_record.features))
+#gb_feature = gb_record.features[1]
+#print gb_feature.extract(gb_record.seq).translate(table=11, cds=True)
+#for seq_record in SeqIO.read(open(sys.argv[1],'r'),'genbank'):
+#    print seq_record
+#    for feat in seq_record.features:
+#        if feat.type == "CDS":
+#            print fet.type
+#            print feat.translation
+for record in gb_record.features:
+    if (record.type == "CDS"):
+        for x in record.qualifiers:
+            if (x == "translation"):
+                ids = [">",record.qualifiers["locus_tag"][0]]
+                print "".join(ids)
+                print record.qualifiers["translation"][0]
+#        print record.extract(gb_record.seq).translate(table=11,cds=True)

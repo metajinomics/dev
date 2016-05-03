85f476ef51ae9badf83072706572b7533aecdd5a
Jin Choi <jinchoi721@gmail.com>
2016-05-03 14:06:16 -0500

    update

---
+++ emp_tools/count_abun_emp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# find abudance 
+# usage: python count_abun_emp.py id summary > abun
+import sys
+sread = open(sys.argv[1],'r')
+sum = {}
+flag = 0
+for line in sread:
+    if flag == 0:
+        if (line[:21] == "Counts/sample detail:"):
+            flag = 1
+        continue
+    spl = line.strip().split(':')
+    sum[spl[0].strip()]=spl[1].strip()
+sread.close()
+
+add = 0
+for line in open(sys.argv[2],'r'):
+    if sum.has_key(line.strip()):
+        add = add + float(sum[line.strip()])
+print add

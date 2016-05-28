4e3e902e966b2af4a7202bd9a360b5b4550df2e5
Jin Choi <jinchoi721@gmail.com>
2016-05-27 22:09:47 -0500

    update

--- amplicon_tools/separate_by_sample.py
+++ amplicon_tools/separate_by_sample.py
@@ -4,10 +4,16 @@
 import sys
 
 
-def check_barcode(seq,dict):
-    if( dict.has_key(seq[1][-12:])):
-        print '\n'.join(seq)
-
+def check_barcode(seq,dict,result):
+    barcode = seq[1][-12:]
+    if( dict.has_key(barcode)):
+        samid = dict[barcode]
+        if (result.has_key(samid)):
+            temp = result[samid]
+            temp.append('\n'.join(seq))
+            result[samid]=temp
+        else:
+            result[samid]=['\n'.join(seq)]
 def main():
     dict = {}
     inforead = open(sys.argv[1],'r')
@@ -20,14 +26,19 @@ def main():
 
     seqread = open(sys.argv[2],'r')
     seq  = [] 
+    result = {}
     for n,line in enumerate(seqread):
         if( n % 4 == 3):
             seq.append(line.strip())
-            check_barcode(seq,dict)
+            check_barcode(seq,dict,result)
             seq = []
         else:
             seq.append(line.strip())
-
+    for item in result.items():
+        fwrite = open(item[0]+".R1.fastq",'w')
+        for x in  item[1]:
+            fwrite.write(x+'\n')
+        fwrite.close()
 
 if __name__ == '__main__':
     main()

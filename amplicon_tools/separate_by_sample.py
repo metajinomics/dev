e534cffec30c4ef72dcb1bd83e2568bca3614bbb
Jin Choi <jinchoi721@gmail.com>
2016-05-27 21:36:28 -0500

    update

---
+++ amplicon_tools/separate_by_sample.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# this script separate fastq file into samples
+#usage: python separate_by_sample.py desciption fastaq 
+import sys
+
+
+def check_barcode(seq,dict):
+    if( dict.has_key(seq[1][-12:])):
+        print '\n'.join(seq)
+
+def main():
+    dict = {}
+    inforead = open(sys.argv[1],'r')
+    for line in inforead:
+        if (line[:1] == "#"):
+            continue
+        else:
+            spl = line.strip().split('\t')
+            dict[spl[3]] = spl[0]
+
+    seqread = open(sys.argv[2],'r')
+    seq  = [] 
+    for n,line in enumerate(seqread):
+        if( n % 4 == 3):
+            seq.append(line.strip())
+            check_barcode(seq,dict)
+            seq = []
+        else:
+            seq.append(line.strip())
+
+
+if __name__ == '__main__':
+    main()

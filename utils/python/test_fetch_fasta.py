219282503596bec4fae73c15df514c2ccd73199d
Jin Choi <jinchoi721@gmail.com>
2016-07-22 17:21:03 -0500

    update

--- utils/python/test_fetch_fasta.py
+++ utils/python/test_fetch_fasta.py
@@ -3,5 +3,5 @@ import sys
 sys.path.append('./utils')
 import fetch_fasta
 
-fetch_fasta.from_blast(sys.argv[1])
+fetch_fasta.from_multi_blast(sys.argv[1])
 
--- utils/python/utils/fetch_fasta.py
+++ utils/python/utils/fetch_fasta.py
@@ -16,3 +16,23 @@ def get_fasta(id,start,end):
     s1 = url_template.format(id,start,end)
     seq = urllib2.urlopen(s1).read()
     return seq
+
+def from_multi_blast(filename):
+    ids = ""
+    flag = 0
+    seqs = []
+    for line in open(filename,'r'):
+        spl = line.strip().split('\t')
+        id = spl[1].split('|')[1]
+        if (spl[0] == ids):
+            seq = get_fasta(id,spl[8],spl[9])
+            seqs.append(seq)
+        elif (flag == 0 and spl[0] != ids):
+            gbk_out_file = os.path.join('./',spl[0]+'.fa')
+            flag = 1
+            ids = spl[0]
+        elif (flag == 1 and spl[0] != ids):
+            open(gbk_out_file, "w").write(''.join(seqs))
+            gbk_out_file = os.path.join('./',spl[0]+'.fa')
+            ids = spl[0]
+    open(gbk_out_file, "w").write(''.join(seqs))

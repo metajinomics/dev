79c566903e5bf003d9ce2c7bf5aa671ecf453168
Jin Choi <jinchoi721@gmail.com>
2016-08-02 16:18:05 -0500

    update

--- primer_design/blast_gi.py
+++ primer_design/blast_gi.py
@@ -5,4 +5,5 @@ import sys
 for line in open(sys.argv[1],'r'):
     spl = line.strip().split('\t')
     ids = spl[1].split('|')[1]
-    print ids+'\t'+spl[8]+'-'+spl[9]
+    if (int(spl[8]) < int(spl[9])):
+        print ids+'\t'+spl[8]+'-'+spl[9]

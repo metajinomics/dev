de4e213738c919b49852dde64a75d49df83f8989
Jin Choi <jinchoi721@gmail.com>
2016-10-24 09:30:52 -0500

    add

---
+++ primer_design/find_primer.py
@@ -0,0 +1,49 @@
+
+#find which primer is used
+#python find_primer.py primer seq
+
+import sys
+import utils
+import reverse_complement
+
+def mismatch(str1, str2, alow):
+    mis = 0
+    for i in range(0,len(str1)):
+        if(str1[i] != str2[i]):
+            mis += 1
+            if (mis > alow):
+                return False
+    return True
+
+def main():
+    #parameters that used
+    mis = 0
+    #read primer
+    fpri, rpri = utils.read_trad_primer(sys.argv[1])
+    
+    seq = utils.read_fasta(sys.argv[2])
+
+    for item in seq.items():
+        se = item[1]
+
+        for f in fpri:
+            #print f
+            for i in range(0, len(se)-len(f)):
+                str1 = se[i:len(f)+i]
+                if mismatch(str1,f,mis):
+                    print ">F:",f, i
+                    for r in rpri:
+                        #print r
+                        #rp = reverse_complement.get_rc(r)
+                        rp = r
+                        #print rp
+                        for j in range(0, len(se)-len(rp)):
+                            str2 = se[j:len(rp)+j]
+                            #print str2
+                            #print rp
+                            if mismatch(str2,rp,mis):
+                                frp = reverse_complement.get_rc(rp)
+                                print ">R:",frp,j+len(rp), j+len(rp)-i
+
+if __name__ == '__main__':
+    main()

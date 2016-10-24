ddeb49d0ac1df35a77ccfa1bc44c3369e0ede332
Jin Choi <jinchoi721@gmail.com>
2016-10-24 09:33:44 -0500

    update

--- primer_design/utils.py
+++ primer_design/utils.py
@@ -1,10 +1,42 @@
 import reverse_complement
 
+def read_trad_primer(filename):
+    fpri = []
+    rpri = []
+    flag = 0
+    for line in open(filename,'r'):
+        if(line[:2] == ">F"):
+            flag = 0
+        elif(line[:2] == ">R"):
+            flag = 1
+        else:
+            if(flag == 0):
+                fpri.append(line.strip())
+            else:
+                seq = reverse_complement.get_rc(line.strip())
+                rpri.append(seq)
+    return fpri,rpri
+
 def read_map(filename):
     dict={}
     for line in open(filename,'r'):
         spl = line.strip().split('\t')
         dict[spl[1]] = spl[0]
+    return dict
+
+def read_check_match(filename):
+    dict ={}
+    name = ''
+    for line in open(filename,'r'):
+        if('.txt' in line):
+            name = line.strip()
+        else:
+            if (dict.has_key(name)):
+                temp = dict[name]
+                temp.append(line.strip())
+                dict[name] = temp
+            else:
+                dict[name] = [line.strip()]
     return dict
 
 def get_target_seq(locus,filename):
@@ -29,12 +61,48 @@ def get_target(fpri,rpri,ori):
             target = item[1]
     return target
 
+def if_amplified(fpri,rpri,seq):
+    target = False
+    if(fpri in seq and rpri in seq):
+        target = True
+    return target
+
+def read_arg_list(filename):
+    li = []
+    for line in open(filename,'r'):
+        spl = line.strip().split('\t')
+        if(len(spl)==3):
+            li.append([spl[0],spl[1].split(':')[0],spl[2].split(':')[0]])
+    return li
+
+def read_arg_list_all(filename):
+    li = []
+    for line in open(filename,'r'):
+        spl = line.strip().split('\t')
+        if(len(spl)==3):
+            li.append([spl[0],spl[1].split(':')[0],spl[2].split(':')[0]])
+        else:
+            li.append([spl[0],spl[1].split(':')[0]])
+    return li
+
+def read_final_list(filename):
+    li = []
+    for n, line in enumerate(open(filename,'r')):
+        if(n == 0):
+            continue
+        spl = line.strip().split('\t')
+        temp = []
+        for x in spl:
+            temp.append(x)
+        li.append(temp)
+    return li
+
 def make_primer3_input(fprimer,rprimer,target,gene,num):
     fwrite = open(num+gene+".primer3input.txt",'w')
     fwrite.write("SEQUENCE_TEMPLATE="+target+'\n')
     fwrite.write("SEQUENCE_PRIMER="+fprimer+'\n')
     fwrite.write("SEQUENCE_PRIMER_REVCOMP="+rprimer+'\n')
-    default = open("/Users/jinchoi/programs/primer3-2.3.7/primer3_default_template.txt",'r')
+    default = open("/Users/jinchoi/programs/primer3-2.3.7/primer3_default_template_truseq.txt",'r')
     for line in default:
         fwrite.write(line)
     return
@@ -54,6 +122,19 @@ def make_primer3_input_design(fp,rp,target,iter):
         fwrite.write(line)
     return
 
+def check_error(filename):
+    outread = open(filename,'r')
+    print filename
+    flag = 0
+    for line in outread:
+        if("ERROR" in line):
+            print "error: "+line
+            flag = 1
+        elif("PROBLEMS" in line):
+            print "problem: "+line
+            flag = 1
+    if (flag == 0):
+        print "no problem"
 
 def read_fasta(filename):
     fread = open(filename,'r')
@@ -63,6 +144,23 @@ def read_fasta(filename):
     for line in fread:
         if(line[:1] == ">"):
             name = line.strip()
+        else:
+            if(dict.has_key(name)):
+                temp = dict[name]
+                temp = temp + line.strip()
+                dict[name] = temp
+            else:
+                dict[name] = line.strip()
+    return dict
+
+def read_fasta_usda(filename):
+    fread = open(filename,'r')
+    dict = {}
+    name = ""
+    flag = 0
+    for line in fread:
+        if(line[:1] == ">"):
+            name = line.strip().split(' ')[0][1:]
         else:
             if(dict.has_key(name)):
                 temp = dict[name]

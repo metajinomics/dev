e30bb10c1519b4088ef405d7b8c3cf9c47c856e5
Jin Choi <jinchoi721@gmail.com>
2016-10-24 09:40:15 -0500

    update

--- primer_design/find_primer.py
+++ primer_design/find_primer.py
@@ -20,12 +20,9 @@ def main():
     mis = 0
     #read primer
     fpri, rpri = utils.read_trad_primer(sys.argv[1])
-    
     seq = utils.read_fasta(sys.argv[2])
-
     for item in seq.items():
         se = item[1]
-
         for f in fpri:
             #print f
             for i in range(0, len(se)-len(f)):
@@ -34,21 +31,15 @@ def main():
                     output = ">F:\n"+f+'\n'
                     #print ">F:",f, i
                     for r in rpri:
-                        
-                        
                         rp = r
-                        
                         for j in range(0, len(se)-len(rp)):
                             str2 = se[j:len(rp)+j]
-                            
-                            
                             if mismatch(str2,rp,mis):
                                 frp = reverse_complement.get_rc(rp)
-                                
                                 #print ">R:",frp,j+len(rp), j+len(rp)-i
                                 flen = j+len(rp)-i
                                 if(flen >250 and flen<320):
-                                    output = output + ">R:\n"+frp+'\n'
+                                    output = output + ">R:\n"+frp
                                     print output
 
 if __name__ == '__main__':

66beef8a16f2040d59cf6713190e0ca3bbfe28f2
Jin Choi <jinchoi721@gmail.com>
2016-10-27 08:37:39 -0500

    update

---
+++ primer_design/replace_N.py
@@ -0,0 +1,12 @@
+#python replace_N.py input > output
+
+#this script replace N into - for fasta file
+
+import sys
+
+for line in open(sys.argv[1],'r'):
+    if (line[:1] == ">"):
+        print line.strip()
+    else:
+        newline = line.strip().replace("N", "-")
+        print newline

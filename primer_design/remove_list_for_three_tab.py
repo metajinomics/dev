fa746855d684d57bda92f9ea33b38ddd32255a8a
Jin Choi <jinchoi721@gmail.com>
2016-11-09 17:09:29 -0600

    update

---
+++ primer_design/remove_list_for_three_tab.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#this script remove list
+#usage: python remove_list.py original_list.txt list_to_remove.txt > new_list.txt
+
+import sys
+
+d = {}
+for line in open(sys.argv[2],'r'):
+    d[line.strip()] = 0
+
+for line in open(sys.argv[1],'r'):
+    spl = line.strip().split('\t')
+    if not d.has_key(spl[2]):
+        print line.strip()

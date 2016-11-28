55aef0c11652889a5245c948129c0dff4a9d8da9
Jin Choi <jinchoi721@gmail.com>
2016-11-28 09:42:06 -0600

    update

---
+++ primer_design/tsv_rev_comp.py
@@ -0,0 +1,12 @@
+
+#python tsv_rev_comp.py input > output
+
+import sys
+import reverse_complement
+
+for line in open(sys.argv[1],'r'):
+    spl = line.strip().split('\t')
+    r = reverse_complement.get_rc(spl[3])
+    spl[3] = r
+    print '\t'.join(spl)
+    

f095c3e40650d4e6d6d8b5e30c0340341468625b
Jin Choi <jinchoi721@gmail.com>
2016-11-30 09:40:37 -0600

    update

--- primer_design/get_fasta_from_design.py
+++ primer_design/get_fasta_from_design.py
@@ -6,14 +6,12 @@ tru = 1
 def main():
     num = 0
     file = sys.argv[1]
-    set = 0
+    #set = 0
     for line in open(file,'r'):
         genename = file.split('.')[0]
         if line[:13] == "Primer Pair: " :
-            set += 1
+            #set += 1
             prim = {}
-            prim['f'] = []
-            prim['r'] = []
             num += 1
             spl = line.strip().split("revOligo")
             for i in range(0,len(spl)):
@@ -25,16 +23,10 @@ def main():
                     if( i == 0):
                         print ">F:"+file+"_"+str(num)+"_"+str(snum)
                         print oneoli[0]
-                        prim['f'].append(oneoli[0])
                     else:
                         print ">R:"+file+"_"+str(num)+"_"+str(snum)
                         print oneoli[0]
-                        prim['r'].append(oneoli[0])
-                    #print oneoli[0]
-            #print prim
-            #for i in range(0,len(prim['f'])):
-                #r = reverse_complement.get_rc(prim['r'][i])
-                #print genename+"\t"+str(set)+"_"+str(i+1)+"\t"+prim['f'][i]+"\t"+ r
+
 
 if __name__ == '__main__':
     main()

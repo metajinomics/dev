330141e62865dd46a5626172574b8269fcbbfaeb
Jin Choi <jinchoi721@gmail.com>
2016-12-14 11:29:19 -0600

    fix bug

--- primer_design/tsv_to_fasta.py
+++ primer_design/tsv_to_fasta.py
@@ -4,9 +4,11 @@
 import sys
 
 for line in open(sys.argv[1],'r'):
+    if line.strip() == "target_gene\ttarget_locus\tF_trad\tR_trad\tProduct_length":
+        continue
     spl = line.strip().split('\t')
-    if not spl[2] == "-":
+    if spl[2] != "-" and spl[2] != "_":
         print ">F:%s_%s\n%s" %(spl[0],spl[1],spl[2])
-    if not spl[3] == "-":
+    if spl[3] != "-" and spl[3] != "_":
         print ">R:%s_%s\n%s" %(spl[0],spl[1],spl[3])
 

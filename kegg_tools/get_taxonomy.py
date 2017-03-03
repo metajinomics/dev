85b359f2b053a6e806788fd5b75d229575126e0b
Jin Choi <jinchoi721@gmail.com>
2017-03-03 10:37:53 -0600

    update

--- kegg_tools/get_taxonomy.py
+++ kegg_tools/get_taxonomy.py
@@ -5,5 +5,7 @@ import taxonomy_finder
 
 for line in open(sys.argv[1],'r'):
     spl = line.strip().split('\t')
+
     tax = taxonomy_finder.get_taxonomy(spl[2])
-    print spl[0]+'\t'+ '\t'.join(tax)
+    #print tax
+    #print spl[0]+'\t'+ '\t'.join(tax)
--- kegg_tools/taxonomy_finder.py
+++ kegg_tools/taxonomy_finder.py
@@ -1,82 +1,126 @@
 #!/usr/bin/python
 # this module return full taxonomy lineage. Cannot handle multiple genbank info in one file. should be one genbank id in each file
-import urllib
-import cStringIO
-import pycurl
 
-def get_taxonomy(taxon):
+#import urllib
+#import cStringIO
+#import pycurl
+
+import urllib2
+
+
+def get_taxonomy(tax_id):
     #flist = [locus,taxon]
+    
     flist = []
-    buf = cStringIO.StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=' + taxon)
-    c.setopt(c.WRITEFUNCTION, buf.write)
-    c.perform()
+    #buf = cStringIO.StringIO()
+    #c = pycurl.Curl()
+    #c.setopt(c.URL, 'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=' + taxon)
+    #c.setopt(c.WRITEFUNCTION, buf.write)
+    #c.perform()
 
-    x= buf.getvalue()
-    dat = x.split('\n')
+    #x= buf.getvalue()
+    #dat = x.split('\n')
 
-    for line in dat:
-        if('<dd><a ALT' in line):
-            taxID=""
-            lenID = len(taxID)
-            lenCut = 163+lenID
+    url_template = "https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=%s"
+    ge = urllib2.urlopen(url_template % tax_id).read()
+    #dat= ge.split('\n')
+
+    #print dat
+    #for line in dat:
+    #    if('<dd><a ALT' in line):
+    taxID=""
+    line = ge
+    lenID = len(taxID)
+    lenCut = 163+lenID
     
-            startK = line.find("TITLE=\"superkingdom\">")
-            startK = startK + 21
-            endK = line.find("</a", startK)
-            if (endK == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startK:endK])
+    Kt = ""
+    Pt = ""
+    Ct = ""
+    Ot = ""
+    Ft = ""
+    Gt = ""
+    St = ""
 
-            startP = line.find("TITLE=\"phylum\">")
-            startP = startP + 15
-            endP = line.find("</a", startP)
-            if (endP == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startP:endP])
-
-            startC = line.find("TITLE=\"class\">")
-            startC = startC + 14
-            endC = line.find("</a", startC)
-            if (endC == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startC:endC])
+    startK = line.find("TITLE=\"superkingdom\">")
+    startK = startK + 21
+    endK = line.find("</a", startK) 
+    #print startK, endK
     
-            startO = line.find("TITLE=\"order\">")
-            startO = startO + 14
-            endO = line.find("</a", startO)
-            if (endO == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startO:endO])
+    if (endK == lenCut):
+        Kt = "null"
+        flist.append("null")
+    else:
+        Kt = line[startK:endK]
+        flist.append(line[startK:endK])
 
-            startF = line.find("TITLE=\"family\">")
-            startF = startF + 15
-            endF = line.find("</a", startF)
-            if (endF == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startF:endF])
+    startP = line.find("TITLE=\"phylum\">")
+    startP = startP + 15
+    endP = line.find("</a", startP) 
+    if (endP == lenCut):
+        Pt = "null"
+        flist.append("null")
+    else:
+        Pt = line[startP:endP]
+        flist.append(line[startP:endP])
 
-            startG = line.find("TITLE=\"genus\">")
-            startG = startG + 14
-            endG = line.find("</a", startG)
-            if (endG == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startG:endG])
+    startC = line.find("TITLE=\"class\">")
+    startC = startC + 14
+    endC = line.find("</a", startC)
+    if (endC == lenCut):
+        Ct = "null"
+        flist.append("null")
+    else:
+        Ct = line[startC:endC]
+        flist.append(line[startC:endC])
+    
+    startO = line.find("TITLE=\"order\">")
+    startO = startO + 14
+    endO = line.find("</a", startO)
+    if (endO == lenCut):
+        Ot = "null"
+        flist.append("null")
+    else:
+        Ot = line[startO:endO]
+        flist.append(line[startO:endO])
 
-            startS = line.find("TITLE=\"species\">")
-            startS = startS + 16
-            endS = line.find("</a", startS)
-            if (endS == lenCut):
-                flist.append("null")
-            else:
-                flist.append(line[startS:endS])
+    startF = line.find("TITLE=\"family\">")
+    startF = startF + 15
+    endF = line.find("</a", startF)
+    if (endF == lenCut):
+        Ft = "null"
+        flist.append("null")
+    else:
+        Ft = line[startF:endF]
+        flist.append(line[startF:endF])
+
+    startG = line.find("TITLE=\"genus\">")
+    startG = startG + 14
+    endG = line.find("</a", startG)
+    if (endG == lenCut):
+        Gt = "null"
+        flist.append("null")
+    else:
+        Gt = line[startG:endG]
+        flist.append(line[startG:endG])
+
+    startS = line.find("TITLE=\"species\">")
+    if startS > 0 :
+        startS = startS + 16
+        endS = line.find("</a", startS)
+        if (endS == lenCut):
+            St = "null"
+            flist.append("null")
+        else:
+            St = line[startS:endS]
+            flist.append(line[startS:endS])
+    else:
+        startS = line.find("Taxonomy browser")
+        endS = line.find(")</title>")
+        #print startS,endS
+        St = line[startS:endS]
+        flist.append(line[startS:endS])
+    flist = [Kt,Pt,Ct,Ot,Ft,Gt,St]
+    print flist
     return flist
 
 def full(filename):

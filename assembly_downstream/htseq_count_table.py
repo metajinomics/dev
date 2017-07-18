a4fcdd3fecbce34110255ec862c4fd6239326184
Jin Choi <jinchoi721@gmail.com>
2017-07-18 09:53:01 -0500

    htseq count

---
+++ assembly_downstream/htseq_count_table.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    list_id = []
+    count = {}
+    for n, filename in enumerate(sys.argv[1:]):
+        for line in open(filename,'r'):
+            spl = line.strip().split('\t')
+            if n == 0:
+                list_id.append(spl[0])
+            if count.has_key(spl[0]):
+                count[spl[0]] = count[spl[0]].append(spl[1])
+            else:
+                count[spl[0]] = [spl[1]]
+    for ids in list_id:
+        print ids+'\t'+'\t'.join(count[ids])
+
+
+if __name__ == '__main__':
+    main()

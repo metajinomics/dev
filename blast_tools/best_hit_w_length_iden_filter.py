a552fee485dba2edd69a4fa4ace8158207751f37
Jin Choi <jinchoi721@gmail.com>
2017-11-06 12:26:34 -0600

    update

--- blast_tools/best_hit_w_length_iden_filter.py
+++ blast_tools/best_hit_w_length_iden_filter.py
@@ -5,10 +5,11 @@
 import sys
 
 filter = int(sys.argv[2])
+iden = float(sys.argv[3])
 prev = ""
 for line in open(sys.argv[1],'r'):
     spl = line.strip().split('\t')
     if not prev == spl[0]:
         prev = spl[0]
-        if int(spl[3]) > filter:
+        if int(spl[3]) > filter and float(spl[2]) > iden:
             print line,

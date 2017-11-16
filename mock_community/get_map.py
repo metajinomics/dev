07342aaa0ab80e227ce2cc65e3589eceb54d6e1b
Jin Choi <jinchoi721@gmail.com>
2017-11-16 14:26:01 -0600

    upadte

--- mock_community/get_map.py
+++ mock_community/get_map.py
@@ -10,16 +10,20 @@ from Bio import SeqIO
 
 def main():
     gb_file = sys.argv[1]
-#    gb_record = SeqIO.read(open(gb_file,'r'),'genbank')
+
     for gb_record in SeqIO.parse(gb_file,"genbank") :
         genome_name = gb_record.annotations["source"]
         for record in gb_record.features:
             if (record.type == "CDS"):
                 for x in record.qualifiers:
                     if (x == "translation"):
-                        ids = [">",record.qualifiers["locus_tag"][0]]
-                        gene_name = "".join(ids)
-                        print '\t'.join([genome_name,gene_name[1:]])
+                        if record.qualifiers.has_key("locus_tag"):
+                            gene_name = record.qualifiers["locus_tag"][0]
+                        elif record.qualifiers.has_key("db_xref"):
+                            gene_name = record.qualifiers["db_xref"][0]
+                        else:
+                            continue
+                        print '\t'.join([genome_name,gene_name])
 
 
 if __name__ == '__main__':

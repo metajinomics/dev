15ef3676fff2cf207dec2f74ca9446d231d24f26
Jin Choi <jinchoi721@gmail.com>
2017-11-16 14:16:48 -0600

    upadte

---
+++ mock_community/get_map.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+"""
+usage: python get_map.py genbank_file > output
+"""
+
+import sys
+from Bio import SeqIO
+
+
+def main():
+    gb_file = sys.argv[1]
+    gb_record = SeqIO.read(open(gb_file,'r'),'genbank')
+    
+    genome_name = gb_record.annotations["source"]
+    for record in gb_record.features:
+        if (record.type == "CDS"):
+            for x in record.qualifiers:
+                if (x == "translation"):
+                    ids = [">",record.qualifiers["locus_tag"][0]]
+                    gene_name = "".join(ids)
+                    genome = gb_file.split('.')[0].split('/')[1]
+                    print '\t'.join([genome_name,gene_name[1:]])
+if __name__ == '__main__':
+    main()

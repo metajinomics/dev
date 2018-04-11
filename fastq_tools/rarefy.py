5056162bac6c10d7e4b0164dcde5a3c2b62d18ea
Jin Choi <jinchoi721@gmail.com>
2018-04-11 08:47:49 -0500

    rarefy fastq

---
+++ fastq_tools/rarefy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python 
+"""
+This script random sampling fastq file
+usage:
+python rarefy.py fastqfile.fastq 3 > output.fastq
+"""
+
+import sys
+import random
+
+def main():
+    #get number of line
+    even_num = int(sys.argv[2])
+    num_lines = sum(1 for line in open(sys.argv[1],'r'))
+
+    num_sets = num_lines/4
+    ran = random.sample(range(0,num_sets),even_num)
+
+    ran4 = []
+    for x in ran:
+        ran4.append(x*4+3)
+
+    temp = []
+    for n, line in enumerate(open(sys.argv[1],'r')):
+        if n % 4 == 3:
+            temp.append(line)
+            if n in ran4:
+                print ''.join(temp),
+            temp = []
+        else:
+            temp.append(line)
+if __name__ == '__main__':
+    main()

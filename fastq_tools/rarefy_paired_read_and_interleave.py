9d74d837ce1b34d5ba1a60baa193ef1e3838d725
Jin Choi <jinchoi721@gmail.com>
2018-04-11 08:57:56 -0500

    add rarefy paired read

---
+++ fastq_tools/rarefy_paired_read_and_interleave.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python 
+"""
+This script interleave two fastq into one fastq
+usage:
+python rarefy_paired_read.py fastq1 fastq2 2 > output.interleaved.fastq
+"""
+
+import sys
+import random
+
+def main():
+    r2 = open(sys.argv[2],'r')
+    even_num = int(sys.argv[3])
+
+    num_lines =sum(1 for line in open(sys.argv[1],'r'))
+
+    num_sets = num_lines/4
+    ran = random.sample(range(0,num_sets),even_num)
+
+    ran4 = []
+    for x in ran:
+        ran4.append(x*4+3)
+
+
+    r1temp = []
+    r2temp = []
+    for n, r1line in enumerate(open(sys.argv[1],'r')):
+        if n % 4 == 3:
+            r1temp.append(r1line)
+            r2temp.append(r2.readline())
+            if n in ran4:
+                print ''.join(r1temp),
+                print ''.join(r2temp),
+            r1temp = []
+            r2temp = []
+        else:
+            r1temp.append(r1line)
+            r2temp.append(r2.readline())
+
+
+if __name__ == '__main__':
+    main()

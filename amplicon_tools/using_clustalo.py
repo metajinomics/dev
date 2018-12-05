753357223252d57c1167d25ffb78f8ae54f752dd
Jin Choi <jinchoi721@gmail.com>
2018-12-05 11:34:23 -0600

    update

---
+++ amplicon_tools/using_clustalo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python 
+"""
+This script do ...
+usage:
+python find_center_seq.py list fasta
+"""
+
+import sys
+import screed
+
+def main():
+    #read fasta
+    
+
+    #read list
+    li = []
+    otus = []
+    seq_list = []
+    for n,line in enumerate(open(sys.argv[1],'r')):    
+        spl = line.strip().split('\t')
+        if n == 0:
+            otus = spl
+        elif n == 1:
+            seq_list = spl
+        else:
+            continue
+
+
+    #find represent
+    for i in range(2,len(otus)):
+        li = seq_list[i].split(',')
+        print otus[i], li
+
+    
+
+if __name__ == '__main__':
+    main()
